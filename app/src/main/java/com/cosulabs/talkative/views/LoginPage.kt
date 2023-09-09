79b160fd0445dcc488d79b3f60972e3eab2b54ea
Can Ersoy <can.ersoy@tum.de>
2023-09-09 18:50:41 +0300

    default login page

--- app/src/main/java/com/cosulabs/talkative/views/LoginPage.kt
+++ app/src/main/java/com/cosulabs/talkative/views/LoginPage.kt
@@ -9,9 +9,9 @@ import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
-import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.OutlinedButton
 import androidx.compose.material3.Text
-import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
@@ -20,7 +20,6 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.platform.LocalConfiguration
 import androidx.compose.ui.text.input.PasswordVisualTransformation
 import androidx.compose.ui.text.input.TextFieldValue
 import androidx.compose.ui.unit.dp
@@ -33,8 +32,6 @@ class LoginPage : ComponentActivity() {
 }
 
 
-@SuppressLint("UnrememberedMutableState")
-@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun LoginView(){
     var usernameController by remember { mutableStateOf(TextFieldValue()) }
@@ -53,9 +50,11 @@ fun LoginView(){
         CustomTextField(
             visualTransformation = PasswordVisualTransformation('*'),
             placeholder = {Text("Password: ", color = Color.White)},
-            modifier = Modifier.padding(bottom = 30.dp),
-            value = passwordController, onValueChange = {passwordController = it})
+            modifier = Modifier.padding(bottom = 30.dp), value = passwordController, onValueChange = {passwordController = it})
         LoginButton(text = "Login", modifier = Modifier.width(BUTTON_WIDTH))
         LoginButton(text = "Register",modifier = Modifier.width(BUTTON_WIDTH))
+        OutlinedButton(onClick = {},modifier = Modifier.padding(top = 20.dp), colors = ButtonDefaults.outlinedButtonColors(containerColor = Color.LightGray, contentColor = Color.DarkGray)) {
+            Text("Forgot Password?",)
+        }
     }
 }
